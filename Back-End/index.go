f0e8ba0f1ea43cb16bd5adf64e6b9f94895ffb11
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2021-06-11 16:35:58 -0500

    Endpoints added

--- Back-End/index.go
+++ Back-End/index.go
@@ -1,9 +1,96 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
+//--------------------------------------------------------
+//						Structures
+//--------------------------------------------------------
+type Product struct {
+	Id, Name string
+	Price    int
+}
+
+type Person struct {
+	Id, Name, Age string
+}
+
+type Transaction struct {
+	Id, BuyerId, Ip, Device string
+	ProductIds              []string
+}
+
+//--------------------------------------------------------
+//						Methods
+//--------------------------------------------------------
+//----------------------- Products -----------------------
+// Function to add a list of products to the DB
+func postProducts(response http.ResponseWriter, request *http.Request) {
+	var product Product
+	err := json.NewDecoder(request.Body).Decode(&product)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprint(response, "Product: %v", product)
+	fmt.Printf("Product : id[%v] name[%v] price[%v]", product.Id, product.Name, product.Price)
+}
+
+//------------------------ Buyers ------------------------
+// Function to add a list of buyers to the DB
+func postBuyers(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("Hello from post buyers"))
+}
+
+// Function to get all the buyers that have been buy in the plattform
+func getBuyers(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("Hello from get buyers"))
+}
+
+// Function to get a buyer given an Id
+func getBuyerById(response http.ResponseWriter, request *http.Request) {
+	// buyerId := chi.URLParam(router)
+	buyerId := "Some ID"
+	fmt.Printf("Buyer Id : %v", buyerId)
+	response.Write([]byte("Hello from get buyer by id"))
+}
+
+//--------------------- Transactions ---------------------
+// Function to add a list of transactions
+func postTransactions(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("addTransactions : Called")
+	response.Write([]byte("Hello from transactions"))
+}
+
+//--------------------------------------------------------
+//						Main
+//--------------------------------------------------------
 func main() {
-	fmt.Println("Initial commit")
+
+	//--------- Chi router ---------
+	// Create chi router
+	router := chi.NewRouter()
+	// Chi middlewares
+	router.Use(middleware.Logger)
+
+	//--------- PRODUCT endpoints ---------
+	router.Post("/products", postProducts) // Add products
+
+	//--------- BUYER endpoints ---------
+	router.Post("/buyers", postBuyers)           // Add buyers
+	router.Get("/buyers", getBuyers)             // Get buyers
+	router.Get("/buyer/{buyerId}", getBuyerById) // Get buyer given an id
+
+	//--------- TRANSACTION endpoints ---------
+	router.Post("/transactions", postTransactions) // Add transactions
+
+	//--------- Listenning server ---------
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
